98a1ae1c10af3e10998846fabe3804ccaf9d21ab
mariaGnlz <mariakg@protonmail.com>
2019-03-25 14:05:33 +0100

    commit prueba

--- NotesB5/app/controllers/users_controller.rb
+++ NotesB5/app/controllers/users_controller.rb
@@ -24,9 +24,11 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    byebug
+    @user=User.new
     @user.admin=false
-
+    @user.name=params[:name]
+    @user.password=params[:password]
 
     if @user.save
       redirect_to root_url, :notice => 'User was successfully created.'
@@ -63,6 +65,7 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
